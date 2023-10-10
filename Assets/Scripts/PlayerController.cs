d69be9a80f39b46e3675d857e7dad3edb8c3d442
bhaddock1 <bhaddock25@sandomenico.org>
2023-10-10 13:57:40 -0700

    2.2.5 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,25 +13,41 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-
+        speed = 3.0f;
+        CenterToEdge = 24f;
     }
 
-    // Update is called once per frame
+    // Call on player movement each frame
     private void Update()
     {
-
+  
+        PlayerMovement();
     }
-
+    //keeps player in bounds of scene and allows player to move left and right
     private void PlayerMovement()
     {
-
+        transform.Translate(Vector3.right * Time.deltaTime * speed * move);
+        if(Mathf.Abs(transform.position.x) > CenterToEdge)
+        {
+            float edge = CenterToEdge;
+            if(transform.position.x < 0)
+            {
+                edge = -CenterToEdge;
+            }
+            transform.position = new Vector3(CenterToEdge, transform.position.y, transform.position.z);
+        }
     }
+
+    // called when move action of playerInputAction is detected. sets the move field to either 1 or -1 based on x value of inputs vector2.
     private void OnMove(InputValue input)
     {
-        
+        Vector2 moveDirection = input.Get<Vector2>();
+        move = moveDirection.x;
+        Debug.Log(move);
     }
+    //fires projectile on fire action
     private void OnFire()
     {
-
+        Instantiate(projectile, transform.position + Vector3.up, projectile.transform.rotation);
     }
 }
